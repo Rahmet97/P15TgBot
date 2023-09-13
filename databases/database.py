2d93b8298b1f46f1348cee98c2f200f94d5ec078
user <user@linux.kali>
2023-09-13 18:02:36 +0500

    Bot ready to deploy..

--- databases/database.py
+++ databases/database.py
@@ -3,10 +3,10 @@ import psycopg2
 
 def con():
     return psycopg2.connect(
-        dbname='p15_tg_bot',
-        user='postgres',
-        password='postgres',
-        host='localhost',
+        dbname='dabs6e5qj7mi83',
+        user='cfzzhyrcmvrsxr',
+        password='2149282f7b998fcbbfec62fe63dd97f9165eb76e746fc3c9ab1ec761b1b1fae4',
+        host='ec2-35-169-9-79.compute-1.amazonaws.com',
         port=5432
     )
 
